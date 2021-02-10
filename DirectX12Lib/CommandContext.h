736601e26f2e723725789afcba3939986d87818a
wudongdong <wudongdong@huya.com>
2021-02-10 11:54:04 +0800

    CommandContext, CommandListManager

--- DirectX12Lib/CommandContext.h
+++ DirectX12Lib/CommandContext.h
@@ -1,1 +1,29 @@
-#pragma once
+﻿#pragma once
+
+struct NonCopyable
+{
+	NonCopyable() = default;
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable& operator=(const NonCopyable) = delete;
+};
+
+class FCommandContext : NonCopyable
+{
+private:
+	FCommandContext(D3D12_COMMAND_LIST_TYPE Type);
+
+	void Reset(void);
+
+public:
+	~FCommandContext();
+
+	void Initialize();
+
+	uint64_t FinishFrame(bool WaitForCompletion = false);
+
+protected:
+	D3D12_COMMAND_LIST_TYPE m_Type;
+
+	LinearAllocator m_CpuLinearAllocator;
+	LinearAllocator m_GpuLinearAllocator;
+};
--- DirectX12Lib/CommandListManager.h
+++ DirectX12Lib/CommandListManager.h
@@ -1,1 +1,35 @@
-#pragma once
+﻿#pragma once
+
+#include "CommandQueue.h"
+
+class FCommandListManager
+{
+	friend class FCommandContext;
+
+public:
+	FCommandListManager();
+	~FCommandListManager();
+
+	void Create(ComPtr<ID3D12Device> Device);
+	void Destroy();
+
+	CommandQueue& GetGraphicsQueue() { return m_GraphicsQueue; }
+	CommandQueue& GetComputeQueue() { return m_ComputeQueue; }
+	CommandQueue& GetCopyQueue() { return m_CopyQueue; }
+
+	CommandQueue& GetQueue(D3D12_COMMAND_LIST_TYPE Type = D3D12_COMMAND_LIST_TYPE_DIRECT);
+
+	void CreateNewCommandList(
+		D3D12_COMMAND_LIST_TYPE Type,
+		ComPtr<ID3D12GraphicsCommandList> List,
+		ComPtr< ID3D12CommandAllocator> Allocator);
+
+	bool IsFenceComplete
+
+
+private:
+	ComPtr<ID3D12Device> Device;
+	CommandQueue m_GraphicsQueue;
+	CommandQueue m_ComputeQueue;
+	CommandQueue m_CopyQueue;
+};
